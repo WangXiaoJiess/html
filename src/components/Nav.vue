221b0e717207cc69a62b0dbebed32c6505b9a4aa
wangxiaojie <7809675+wang_xiaojie_1@user.noreply.gitee.com>
2020-08-25 16:30:39 +0800

    完成配置

---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="nav">
+    <img src="http://localhost:8080/%E8%AE%BE%E8%AE%A1%E5%9B%BE/%E5%88%87%E5%9B%BE/logo.png" alt />
+    <span>{{title}}</span>
+    <div>
+      <span>
+        <i class="iconfont icon-user"></i>
+        我的
+      </span>
+      <span>
+        <i class="iconfont icon-nav"></i>
+        导航
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  height: 1rem;
+  align-items: center;
+}
+.nav img {
+    height: 1rem;
+}
+.iconfont {
+  font-size: 0.2rem;
+}
+</style>
